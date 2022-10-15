91c5c99d6927e643b48c6367d1b44653e7124184
chris <christian.schwind@smail.th-koeln.de>
2022-10-15 15:10:33 +0200

    updated tests

--- test/GovernorTest.ts
+++ test/GovernorTest.ts
@@ -1,7 +1,7 @@
 import { time, loadFixture } from "@nomicfoundation/hardhat-network-helpers";
 import { anyValue } from "@nomicfoundation/hardhat-chai-matchers/withArgs";
 import { expect } from "chai";
-import { ethers } from "hardhat";
+import hardhat, { ethers } from "hardhat";
 
 describe("SigningParty", function () {
     const hardhat = require("hardhat")
@@ -19,11 +19,11 @@ describe("SigningParty", function () {
         const governor = await Governor.deploy(governanceToken.address);
 
         // Distribute governance tokens, 100 each user
-        //const votes = ethers.utils.parseUnits("100.0", 18); //???
-        const votes = 100;
-        await governanceToken.mint(userA.address, votes);
-        await governanceToken.mint(userB.address, votes);
-        await governanceToken.mint(userC.address, votes);
+        //const amountOfVotes = ethers.utils.parseUnits("100.0", 18); //???
+        const amountOfVotes = 100;
+        await governanceToken.mint(userA.address, amountOfVotes);
+        await governanceToken.mint(userB.address, amountOfVotes);
+        await governanceToken.mint(userC.address, amountOfVotes);
 
         // Create new proposal
         const grant = ethers.utils.parseUnits("500.0", 18);
@@ -33,7 +33,7 @@ describe("SigningParty", function () {
             descriptionHash: ethers.utils.id("Proposal #2: Give owner some tokens")
         };
 
-        const proposeTx = await governor.connect(userA).propose(
+        const proposeTx = await governor.propose(
             [governanceToken.address],
             [0],
             [newProposal.transferCalldata],
@@ -41,18 +41,30 @@ describe("SigningParty", function () {
         );
 
         const tx = await proposeTx.wait();
-        await hardhat.network.provider.send('evm_mine'); // wait 1 block before opening voting
+        // @ts-ignore
         const proposalId = tx.events.find((e) => e.event == 'ProposalCreated').args.proposalId;
-/*
+
+        // need to wait for one block before voting has opened
+        await hardhat.network.provider.send('evm_mine');
+
+        await governanceToken.connect(userA).delegate(userA.address);
+        await governanceToken.connect(userB).delegate(userB.address);
+        await governanceToken.connect(userC).delegate(userC.address);
+        await governanceToken.delegate(owner.address);
+
+        console.log(await governanceToken.getVotes(userA.address));
+        console.log(await governanceToken.getVotes(userB.address));
+        console.log(await governanceToken.getVotes(userC.address));
+
         // vote
-        await governor.connect(userA).castVote(proposalId, VoteType.For);
-        await governor.connect(userB).castVote(proposalId, VoteType.For);
-        await governor.connect(userC).castVote(proposalId, VoteType.Against);
-        await governor.connect(owner).castVote(proposalId, VoteType.Abstain);
+        await governor.connect(userA).castVote(proposalId, 0);
+        await governor.connect(userB).castVote(proposalId, 1);
+        await governor.connect(userC).castVote(proposalId, 2);
+        await governor.connect(owner).castVote(proposalId, 2);
 
-        const votes = await governor.proposalVotes(proposalId);
-        assert(votes.forVotes.eq(2), "Vote count mismatch"); // < FAILS votes is an array and all its members, "forVotes", "againstVotes", etc are all 0
+        const votingResults = await governor.proposalVotes(proposalId);
 
+/*
         // Exec
         await governor.execute(
             [governanceToken.address],
@@ -61,21 +73,19 @@ describe("SigningParty", function () {
             newProposal.descriptionHash,
         );
 */
-        return { governor, governanceToken, votes, owner, userA, userB, userC };
+        return { governor, governanceToken, amountOfVotes, votingResults, owner, userA, userB, userC , proposalId};
     }
 
-    describe("Deployment", function () {
-        it("Should set the right votes", async function () {
-            const { governanceToken, userA, votes } = await loadFixture(deployBasics);
-
+    describe("Minting", function () {
+        it("Should set the right amount of votes", async function () {
+            const { governanceToken, userA, amountOfVotes } = await loadFixture(deployBasics);
             expect(await governanceToken.balanceOf(userA.address)).to.equal(100);
         });
-
-        /*it("Should set the right owner", async function () {
-            const { lock, owner } = await loadFixture(deployBasics);
-
-            expect(await lock.owner()).to.equal(owner.address);
+    });
+    describe("Voting", function () {
+        it("Should return voting results correctly", async function () {
+            const { governor, proposalId, votingResults } = await loadFixture(deployBasics);
+            expect(votingResults.forVotes).to.equal(2);
         });
-        */
     });
 });
