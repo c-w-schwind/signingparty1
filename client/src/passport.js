a80b259763e8e6c67546aff33eb6b283e229567e
Anthony Pierre Aflatoun <anthony_pierre.aflatoun@smail.th-koeln.de>
2022-10-17 14:54:50 +0200

    [WIP] feat: Gitcoin Passport integration for Sybil-resistance

--- client/src/passport.js
+++ client/src/passport.js
@@ -8,20 +8,27 @@ const verifier = new PassportVerifier();
 
 //TODO: fill stamps & their respective scores
 const criteria = [
-    {},
+    { //Example Stamp
+        provider: "BrightID",
+        issuer: "did:key:z6MkghvGHLobLEdj1bgRLhS4LPGJAvbMA1tn2zcRyqmYU5LC",
+        score: 0.5
+    },
     {},
     {}
 ];
+
+
 const scorer = new PassportScorer(criteria);
 
 
-//TODO: fill the passport address
+//TODO: fill the passport address (msg.sender)
 const address = "0x1234...";
 
 const passport = await reader.getPassport(address);
 const verifiedPassport = await verifier.verifyPassport(address, passport);
 const score = await scorer.getScore(verifiedPassport);
 
+const verified = checkScore(score, 5);
 
 
 //TODO: Specify the threshold a user has to reach to be considered a verified user
