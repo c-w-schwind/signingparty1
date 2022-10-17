7375f1d718fcad4c6bf6383171232e834c11d41d
Anthony Pierre Aflatoun <anthony_pierre.aflatoun@smail.th-koeln.de>
2022-10-17 14:08:13 +0200

    [WIP] feat: Gitcoin Passport integration for Sybil-resistance

--- client/src/passport.js
+++ client/src/passport.js
@@ -5,6 +5,8 @@ import PassportScorer from "@gitcoinco/passport-sdk-scorer"
 const reader = new PassportReader("<https://ceramic.passport-iam.gitcoin.co>", "1");
 const verifier = new PassportVerifier();
 
+
+//TODO: fill stamps & their respective scores
 const criteria = [
     {},
     {},
@@ -13,30 +15,16 @@ const criteria = [
 const scorer = new PassportScorer(criteria);
 
 
-const express = require('express');
-const app = express();
-const port = 3000;
+//TODO: fill the passport address
+const address = "0x1234...";
 
-app.get('/:address', async (req, res) => {
-    const address = req.params.address;
-
-    const passport = await reader.getPassport(address);
-    const verifiedPassport = await verifier.verifyPassport(address, passport);
-    const score = await scorer.getScore(verifiedPassport);
-
-    if(score > 50) {
-        res.send(true);
-    } else {
-        res.send(false);
-    }
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
+const passport = await reader.getPassport(address);
+const verifiedPassport = await verifier.verifyPassport(address, passport);
+const score = await scorer.getScore(verifiedPassport);
 
 
 
-
-
-
+//TODO: Specify the threshold a user has to reach to be considered a verified user
+function checkScore(score, threshold) {
+    return score > threshold;
+}
