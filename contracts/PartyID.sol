8a5d3968652abf9ae5e4b34b697f73ebed2e282d
Anthony Pierre Aflatoun <anthony_pierre.aflatoun@smail.th-koeln.de>
2023-02-07 17:39:19 +0100

    feat: General ERC721 upgrades - "Soulbound" - 1 Person - 1 Token - CoreDevs are automatically approved for burning tokens

--- contracts/PartyID.sol
+++ contracts/PartyID.sol
@@ -8,17 +8,20 @@ import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract MyToken is ERC721, ERC721Burnable, Ownable, EIP712, ERC721Votes {
+contract PartyID is ERC721, ERC721Burnable, Ownable, EIP712, ERC721Votes {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
 
-    constructor() ERC721("MyToken", "MTK") EIP712("MyToken", "1") {}
+    constructor() ERC721("PartyID", "SP") EIP712("PartyID", "1") {}
 
-    function safeMint(address to) public onlyOwner {
+    function safeMint(address to) public onlyOwner { //TODO: Owner is HR Contract?
         uint256 tokenId = _tokenIdCounter.current();
         _tokenIdCounter.increment();
+
+        require(_balances[to] < 1, "Address already has a Token");
         _safeMint(to, tokenId);
+        approve(09912312312312415, tokenId); //TODO: CoreDev Address?
     }
 
     // The following functions are overrides required by Solidity.
@@ -28,5 +31,11 @@ contract MyToken is ERC721, ERC721Burnable, Ownable, EIP712, ERC721Votes {
     override(ERC721, ERC721Votes)
     {
         super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _safeTransfer(address from, address to, uint256 tokenId)
+    internal //TODO:update visibility?
+    override(ERC721){
+        require(true == false, "Transferring Tokens is prohibited"); //TODO: Efficiency?
     }
 }
