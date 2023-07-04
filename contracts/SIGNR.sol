9cef8bac303c490b3efa5efbc267e35ed135fe76
chris <christian.schwind@smail.th-koeln.de>
2023-07-04 21:00:59 +0200

    feat: updated SIGNR contract to include enumerability & include updatable tokenURI

--- contracts/SIGNR.sol
+++ contracts/SIGNR.sol
@@ -2,18 +2,22 @@
 pragma solidity ^0.8.9;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Enumerable.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/ERC721Burnable.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/utils/cryptography/draft-EIP712.sol";
 import "@openzeppelin/contracts/token/ERC721/extensions/draft-ERC721Votes.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
-contract SIGNR is ERC721, ERC721Burnable, Ownable, EIP712, ERC721Votes {
+contract SIGNR is ERC721, ERC721Enumerable, ERC721Burnable, Ownable, EIP712, ERC721Votes {
     using Counters for Counters.Counter;
 
     Counters.Counter private _tokenIdCounter;
+    string private _tokenURI;
 
-    constructor() ERC721("SIGNR", "SIGNR") EIP712("SIGNR", "1") {}
+    constructor(string memory tokenURI_) ERC721("SIGNR", "SIGNR") EIP712("SIGNR", "1") {
+        _tokenURI = tokenURI_;
+    }
 
     function safeMint(address to) public onlyOwner {
         require(balanceOf(to) < 1, "Address already has a Token");
@@ -23,23 +27,33 @@ contract SIGNR is ERC721, ERC721Burnable, Ownable, EIP712, ERC721Votes {
         _safeMint(to, tokenId);
     }
 
-    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize)
-    internal
-    override(ERC721, ERC721Votes) {
-        super._afterTokenTransfer(from, to, tokenId, batchSize);
-    }
-
-
     function burn(uint256 tokenId) public override (ERC721Burnable) {
         require(msg.sender == owner() || _isApprovedOrOwner(_msgSender(), tokenId), "ERC721: caller is not token owner or approved");
         _burn(tokenId);
     }
 
-    function _transfer(
-        address from,
-        address to,
-        uint256 tokenId
-    ) internal override (ERC721) {
+    function _beforeTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721, ERC721Enumerable) {
+        super._beforeTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _afterTokenTransfer(address from, address to, uint256 tokenId, uint256 batchSize) internal override(ERC721, ERC721Votes) {
+        super._afterTokenTransfer(from, to, tokenId, batchSize);
+    }
+
+    function _transfer(address from, address to, uint256 tokenId) internal override (ERC721) {
         require(true == false, "Token is soulbound, transferring prohibited");
+    }
+
+    function setTokenURI(string memory newTokenURI) public onlyOwner {
+        _tokenURI = newTokenURI;
+    }
+
+    function tokenURI(uint256 tokenId) public view virtual override returns (string memory) {
+        _requireMinted(tokenId);
+        return _tokenURI;
+    }
+
+    function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+        return super.supportsInterface(interfaceId);
     }
 }
